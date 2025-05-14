6543d2eb997a1d5cda40b63076cbbe3ffae57676
Plai101 <sirapobboonmun@gmail.com>
2025-05-14 13:06:47 +0700

    Update ddl_gold.sql

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -1,6 +1,6 @@
-# ============================
+# =============================================================
 # Create Dimension : gold.dim_customers
-# ============================
+# =============================================================
 CREATE OR REPLACE VIEW gold.dim_customers AS
 SELECT
 	ROW_NUMBER() OVER (ORDER BY cst_id) AS customer_key,
@@ -21,9 +21,9 @@ ON	      ci.cst_key = ca.cid
 LEFT JOIN silver.erp_loc_a101 AS la
 ON	      ci.cst_key = la.cid;
 
-# ============================
+# =============================================================
 # Create Dimension : gold.dim_products
-# ============================
+# =============================================================
 CREATE OR REPLACE VIEW gold.dim_products AS
 SELECT
 	ROW_NUMBER() OVER (ORDER BY pn.prd_start_dt, pn.prd_key) AS product_key,
@@ -42,9 +42,9 @@ LEFT JOIN silver.erp_px_cat_g1v2 AS pc
 ON pn.cat_id = pc.id
 WHERE prd_end_dt IS NULL; # Filter out all historical data
 
-# ============================
+# =============================================================
 # Create Fact Table : gold.fact_sales
-# ============================
+# =============================================================
 CREATE OR REPLACE VIEW gold.fact_sales AS
 SELECT
 sd.sls_ord_num AS order_number,
