209efaf3d95b9bb7648e9b3d513275e49fce39a1
Sirapop Boonmun <sirapobboonmun@gmail.com>
2025-05-22 15:04:46 +0700

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -78,12 +78,7 @@ WITH cleaned_data AS (
       ELSE sls_price
     END AS fixed_price
   FROM bronze.crm_sales_details
-  WHERE 
-    sls_sales != sls_quantity * sls_price
-    OR sls_sales IS NULL OR sls_quantity IS NULL OR sls_price IS NULL
-    OR sls_sales <= 0 OR sls_quantity <= 0 OR sls_price <= 0
 )
- 
 SELECT
   sls_ord_num,
   sls_prd_key,
